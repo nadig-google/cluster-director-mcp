6726e142523e0c674b7b9d665790f88ca701768b
Rohit Nadig <nadig@google.com>
2025-08-06 02:13:13 +0000

    update

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ echo "--- Installing npm ---"
 
 echo "--- Installing gemin-cli ---"
 export PATH=$PATH:/opt/gradle/bin:/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin:/usr/local/node_packages/node_modules/.bin:/usr/local/rvm/bin:/home/nadig/.gems/bin:/usr/local/rvm/bin:/home/nadig/gopath/bin:/google/gopath/bin:/google/flutter/bin:/usr/local/nvm/versions/node/v22.17.1/bin; 
-npm install -g @google/gemini-cli
-npm install -g @google/gemini-cli to update
+/usr/local/nvm/versions/node/v22.17.1/bin/npm install -g @google/gemini-cli
+/usr/local/nvm/versions/node/v22.17.1/bin/npm install -g @google/gemini-cli to update
 
 echo "--- All tasks complete ---"
 EOF
