48b83d8e26ec079642ccf514bf78aebcb830e84b
Rohit Nadig <nadig@google.com>
2025-08-06 04:14:09 +0000

    updated

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "--- Updating package lists ---"
 apt update -y
 
 echo "--- Upgrading packages ---"
-apt upgrade -y
+apt upgrade google-cloud-cli -y
 
 echo "--- Installing gemin-cli ---"
 export PATH=$PATH:/opt/gradle/bin:/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin:/usr/local/node_packages/node_modules/.bin:/usr/local/rvm/bin:/home/nadig/.gems/bin:/usr/local/rvm/bin:/home/nadig/gopath/bin:/google/gopath/bin:/google/flutter/bin:/usr/local/nvm/versions/node/v22.17.1/bin; 
@@ -19,13 +19,13 @@ EOF
 go build -o cluster-director-mcp .
 ./cluster-director-mcp install gemini-cli
 echo '
-   {
-    "selectedAuthType": "cloud-shell",
-    "theme": "Default",
-    "mcpServers": {
-       "context7": {
-         "httpUrl": "https://mcp.context7.com/mcp"
-        }
-    }
-   } ' >> .gemini/extensions/cluster-director-mcp/gemini-extension.json
+{
+  "selectedAuthType": "cloud-shell",
+  "theme": "Default",
+  "mcpServers": {
+     "context7": {
+       "httpUrl": "https://mcp.context7.com/mcp"
+      }
+  }
+} ' >> .gemini/extensions/cluster-director-mcp/gemini-extension.json
 
